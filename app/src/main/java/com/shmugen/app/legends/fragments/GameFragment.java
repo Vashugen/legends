c8289b55a93abdf2ce062d06f060609f5dffe8c3
Marina Mironova <vashu_gen@mail.ru>
2021-12-13 19:20:12 +0300

    gameContainer + add pics

--- app/src/main/java/com/shmugen/app/legends/fragments/GameFragment.java
+++ app/src/main/java/com/shmugen/app/legends/fragments/GameFragment.java
@@ -10,6 +10,7 @@ import android.widget.ImageView;
 import androidx.fragment.app.Fragment;
 
 import com.shmugen.app.legends.R;
+import com.shmugen.app.legends.common.Shared;
 import com.shmugen.app.legends.engine.Engine;
 import com.shmugen.app.legends.model.Game;
 import com.shmugen.app.legends.ui.BoardView;
@@ -22,12 +23,14 @@ public class GameFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         ViewGroup view = (ViewGroup) inflater.inflate(R.layout.game_fragment, container, false);
-        FrameLayout gameContainer = (FrameLayout) view.findViewById(R.id.game_container);
+        ImageView picsContainer = (ImageView) view.findViewById(R.id.pics_container);
+        ImageView textContainer = (ImageView) view.findViewById(R.id.text_container);
 
-        ViewGroup game = (ViewGroup) LayoutInflater.from(getActivity().getApplicationContext()).inflate(R.layout.game_view, view);
-        
-        gameContainer.addView(game);
+        int picsId = Shared.context.getResources().getIdentifier("pics_1", "mipmap", Shared.context.getPackageName());
+        int textId = Shared.context.getResources().getIdentifier("text_1", "mipmap", Shared.context.getPackageName());
 
+        picsContainer.setImageResource(picsId);
+        textContainer.setImageResource(textId);
 
         return view;
     }
